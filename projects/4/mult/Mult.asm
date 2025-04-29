910482e55fe353e80b51fe98d2f79e1435125451
iineineno03k <iineineno03k@gmail.com>
2025-04-29 20:54:16 +0900

    make Mult

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,59 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+//// 疑似コード
+//    i = R1
+//    sum=0
+//LOOP:
+//    if(i == 0) goto STOP
+//    sum = sum + R0
+//    i = i - 1
+//    goto LOOP
+//STOP:
+//    R2 = sum
+
+
+    // i = R1
+    @R1
+    D=M
+    @i
+    M=D
+    // sum=0
+    @sum
+    M=0
+
+// RAM1の値だけR0の加算をループする。
+(LOOP)
+    // if(i == 0) goto STOP
+    @i
+    D=M
+    @STOP
+    D;JEQ
+
+    // sum = sum + R0
+    @sum
+    D=M
+    @R0
+    D=D+M
+    @sum
+    M=D
+
+    // i = i - 1
+    @i
+    M=M-1
+    
+    // goto LOOP
+    @LOOP
+    0;JMP
+
+(STOP)
+    // R2 = sum
+    @sum
+    D=M
+    @R2
+    M=D
+(END)
+    @END
+    0;JMP
+
+
