5f528afcfa39ddbf2032382524a2a1444f293bc4
iineineno03k <iineineno03k@gmail.com>
2025-04-29 23:04:27 +0900

    Fill途中まで

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,85 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+//// 疑似コード
+//KEYBOARD
+//    i = 0
+//    j = 0
+//    if(RAM[KBD] == 0) goto RESET
+//    if(RAM[KBD] != 0) goto BLACK
+//
+//RESET:
+//    if(i == 8225) goto KEYBOARD
+//    RAM[SCREEN + i] = 0
+//    i = i + 1
+//    goto RESET
+//
+//BLACK:
+//    if(j == 8225) goto KEYBOARD
+//    RAM[SCREEN + j] = -1
+//    j = j + 1
+//    goto BLACK
+
+(KEYBOARD)
+    // i = 0
+    @i
+    M=0
+    // j = 0
+    @j
+    M=0
+    // if(RAM[KBD] == 0) goto RESET
+    @KBD
+    D=M
+    @RESET
+    D;JEQ
+    // if(RAM[KBD] != 0) goto BLACK
+    @BLACK
+    D;JNE
+(RESET)
+    // if(i == 8225) goto KEYBOARD
+    @i
+    D=M
+    @8225
+    D=D-A
+    @KEYBOARD
+    D;JEQ
+    // RAM[SCREEN + i] = 0
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    @address
+    M=D
+    @address
+    A=M
+    M=0
+    // i = i + 1
+    @i
+    M=M+1
+    // goto RESET
+    @RESET
+    0;JMP                                            
+(BLACK)
+    // if(j == 8225) goto KEYBOARD
+    @j
+    D=M
+    @8225
+    D=D-A
+    @KEYBOARD
+    D;JEQ
+    // RAM[SCREEN + j] = -1
+    @SCREEN
+    D=A
+    @j
+    D=D+M
+    @address
+    M=D
+    @address
+    A=M
+    M=-1
+    // j = j + 1
+    @j
+    M=M+1
+    // goto BLACK
+    @BLACK
+    0;JMP
