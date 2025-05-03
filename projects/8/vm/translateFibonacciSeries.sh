dc75224ad8855dc75a4925de086c9e2a488def21
iineineno03k <iineineno03k@gmail.com>
2025-05-04 04:55:25 +0900

    confirm FibonacciSeries

---
+++ projects/8/vm/translateFibonacciSeries.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# binディレクトリがなければ作成
+mkdir -p bin
+
+# VMトランスレータをコンパイル
+javac -d bin src/vm/parser/Parser.java src/vm/codewriter/CodeWriter.java src/vm/VMTranslator.java
+
+# SimpleAdd.vmを変換
+java -cp bin vm.VMTranslator ../ProgramFlow/FibonacciSeries/FibonacciSeries.vm
+
+# 変換結果を表示
+echo "変換完了！"
+echo "生成されたファイル: ../ProgramFlow/FibonacciSeries/FibonacciSeries.asm" 
